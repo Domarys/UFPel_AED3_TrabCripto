bf53ed12d6e7b1433dc06e53de225a6abd97abe6
Domarys <domaryscorrea@gmail.com>
2019-06-05 15:01:45 -0300

    atualizado codigos

--- Gerador/generatorEncoderComparator.py
+++ Gerador/generatorEncoderComparator.py
@@ -7,8 +7,8 @@ import hashlib
 #key = "b'FnAmZZ+NfRVWmY16yvI+aV4s0ztDLiUx3uHtFNHd3iw='" #teste com "domarys9"
 key = "b'YTEUCxI8/wSjrTpm/EqL7s9aR++fqs2f3JBav3vK5Uk='" #palavra-chave
 
-
-permsList = [''.join(x) for x in product((string.ascii_lowercase+string.ascii_uppercase+string.digits), repeat=1)] #indo de 1-8 caracteres
+repeat = 1;#indo de 1-8 caracteres
+permsList = [''.join(x) for x in product((string.ascii_lowercase+string.ascii_uppercase+string.digits), repeat=repeat)] 
 '''
 print(permsList)
 x = "domarys9";
@@ -21,14 +21,15 @@ if str(key64) == str(key):
 	print("achamos")
 '''
 for x in permsList:
+	string = x;
 	x = x.encode("utf-8")
 	key256  = hashlib.sha256(x).digest()
 	key64 = base64.b64encode(key256)
 	if str(key64) == str(key):
-		code = key64;
-		print("achamos")
+		print('codigo',code,'\n','palavra',string,'\n')
 		break
-print(code);
+print('Não foi encontrado a chave para repeat:',repeat);
+
 		
 
 
